e6a24083d6f722d36439470fe42b9bc0370dcb0e
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2024-01-14 05:19:46 +0200

    documentation for main class

--- solitaire.py
+++ solitaire.py
@@ -2,6 +2,15 @@ from game import Game
 
 
 def main():
+    """
+    Main function to run the game.
+
+    This function creates an instance of the Game class, initializes the game slots and card deck,
+    deals the cards, and then runs the game.
+
+    Returns:
+        None
+    """
     game = Game()
     game.create_slots()
     game.create_card_deck()
